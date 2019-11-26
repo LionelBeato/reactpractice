7079a78de827c492516514b41aac6ba0cd88e9e3
LionelBeato <lionel.beato@gmail.com>
2019-11-26 17:49:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Greeting({ name }) {
+  return <div>Hi {name}!</div>;
 }
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+        answer: "Yes",
+        count: 1
+    }
+    this.handleClick = this.handleClick.bind(this)
+
+
+}
+
+handleClick(){
+  this.setState((prevState) => {
+      return {
+          count: prevState.count + 1
+      }
+  })
+}
+
+
+
+  render() {
+    var names = ['Mike', "Adam", 'Bob']
+
+    return (
+      <div className="App">
+        <h1>Is State important to know? {this.state.answer}</h1>
+        <ul>
+          {names.map (function (name){
+          return <li>{name}</li>
+          })}
+        </ul>
+        <Greeting  name="bob"></Greeting>
+
+        <h1>{this.state.count}</h1>
+        <img onMouseOver={() => console.log("Hovered!")} 
+        src="https://www.fillmurray.com/200/100"/>
+        <button onClick={this.handleClick} 
+        onMouseOver={() => console.log("I was hovered!")}>click me</button>
+        
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
