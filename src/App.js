c5e840140f8e227050cc3598c684c0bffa67c715
LionelBeato <lionel.beato@gmail.com>
2019-12-04 16:40:35 -0500

    rejiggers the app to implement crud down the line

--- src/App.js
+++ src/App.js
@@ -1,72 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Head from './header.js';
+import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Paper from './components/Paper';
 
-//simple greeting function with prop destructuring 
-function Greeting({ name }) {
-  return <div>Hi {name}!</div>;
+//test data
+const testData = [
+  {fruitName: "Apple", fruitQuantity: 3},
+  {fruitName: "Pear", fruitQuantity: 2},
+  {fruitName: "Mango", fruitQuantity: 5},
+]
+
+
+
+
+//Arrow notation for the app class
+//Why use arrow notations? One reason is that
+//the keyword "this" beomes less ambiguous. 
+const App = () => {
+
+  return (
+    <div>
+      <Header></Header>
+      <Paper></Paper>
+    </div>
+  )
+
+  
 }
-
-function Fruit({ name, color, quantity}){
-return <div>{name}, {color}, {quantity}</div>
-}
-
-class App extends React.Component {
-
-
-  constructor() {
-    super()
-    this.state = {
-        answer: "Yes",
-        count: 1
-    }
-    //whenever you have an event function you have to bind it
-    //to the "this" object because JavaScript is a cartoon language
-    //thus, the monstrosity below...
-    this.handleClick = this.handleClick.bind(this)
-}
-
-handleClick(){
-  //this function uses setState to alter a property of the class
-  //in this case, the property 
-  this.setState((prevState) => {
-      return {
-          count: prevState.count + 1
-      }
-  })
-}
-
-
-
-
-  render() {
-    var names = ['Mike', "Adam", 'Bob']
-
-
-    return (
-      <div>
-        <Head></Head>
-        <h1>Is State important to know? {this.state.answer}</h1>
-        <ul>
-          {names.map (function (name){
-          return <li>{name}</li>
-          })}
-        </ul>
-        <Fruit name="apple" color="red" quantity="3"></Fruit>
-        <Greeting  name="bob"></Greeting>
-
-        <h1>{this.state.count}</h1>
-        <img onMouseOver={() => console.log("Hovered!")} 
-        src="https://www.fillmurray.com/200/100"/>
-        <button onClick={this.handleClick} 
-        onMouseOver={() => console.log("I was hovered!")}>click me</button>
-
-      </div>
-    );
-  }
-}
-
-
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import AppBar from '@material-ui/core'
+
+const Header = () => {
+
+
+    return (
+
+        <Navbar className="shadow-m" bg="dark" variant="dark">
+        <Navbar.Brand>Test</Navbar.Brand>
+        </Navbar>
+
+    )
+
+}
+
+
+
+
+export default Header;
